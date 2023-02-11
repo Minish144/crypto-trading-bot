ba5f4e2914d79b7b7b51cd6e04e1b2e53f87c75f
Nikolai Varlamov <varlamow.col@yahoo.com>
2023-02-12 01:41:59 +0300

    [feature/macd-strategy] price fix and quantity fix

--- strategies/gridStrategy/algorithm.go
+++ strategies/gridStrategy/algorithm.go
@@ -207,14 +207,14 @@ func (s *GridStrategy) placeSellOrders(ctx context.Context, levels []float64, pr
 	// placing sell orders at each grid level
 	for i, gridLevel := range levels {
 		// calculate the size of the order
-		if err := s.client.NewLimitSellOrder(ctx, s.cfg.Symbol, gridLevel, quantity*(1+s.cfg.GridStep)); err != nil {
+		if err := s.client.NewLimitSellOrder(ctx, s.cfg.Symbol, gridLevel, quantity*(1+float64(i)*s.cfg.GridStep)); err != nil {
 			s.z.Warnw(
 				"failed to place order",
 				"side", "sell",
 				"type", "limit",
 				"multiplier", (1 + s.cfg.GridSize + (float64(i) * s.cfg.GridSize)),
-				"price", gridLevel*(1+s.cfg.GridStep),
-				"quantity", quantity*(1+s.cfg.GridStep),
+				"price", gridLevel,
+				"quantity", quantity*(1+float64(i)*s.cfg.GridStep),
 				"error", err.Error(),
 			)
 
@@ -227,7 +227,7 @@ func (s *GridStrategy) placeSellOrders(ctx context.Context, levels []float64, pr
 			"type", "limit",
 			"multiplier", (1 + s.cfg.GridSize + (float64(i) * s.cfg.GridSize)),
 			"price", gridLevel,
-			"quantity", quantity*(1+s.cfg.GridStep),
+			"quantity", quantity*(1+float64(i)*s.cfg.GridStep),
 		)
 	}
 }
@@ -236,14 +236,14 @@ func (s *GridStrategy) placeBuyOrders(ctx context.Context, levels []float64, pri
 	// placing buy orders at each grid level
 	for i, gridLevel := range levels {
 		// calculate the size of the order
-		if err := s.client.NewLimitBuyOrder(ctx, s.cfg.Symbol, gridLevel, quantity*(1+s.cfg.GridStep)); err != nil {
+		if err := s.client.NewLimitBuyOrder(ctx, s.cfg.Symbol, gridLevel, quantity*(1+float64(i)*s.cfg.GridStep)); err != nil {
 			s.z.Warnw(
 				"failed to place order",
 				"side", "buy",
 				"type", "limit",
 				"multiplier", (1 - (float64(i) * s.cfg.GridSize)),
 				"price", gridLevel,
-				"quantity", quantity*(1+s.cfg.GridStep),
+				"quantity", quantity*(1+float64(i)*s.cfg.GridStep),
 				"error", err.Error(),
 			)
 
@@ -256,7 +256,7 @@ func (s *GridStrategy) placeBuyOrders(ctx context.Context, levels []float64, pri
 			"type", "limit",
 			"multiplier", (1 - (float64(i) * s.cfg.GridSize)),
 			"price", gridLevel,
-			"quantity", quantity,
+			"quantity", quantity*(1+float64(i)*s.cfg.GridStep),
 		)
 	}
 }
